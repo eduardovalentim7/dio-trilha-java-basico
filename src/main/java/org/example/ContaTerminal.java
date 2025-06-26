e217617344f05ab9d49c6471a8b66aeb683fe0f8
eduardovalentim7 <eduardovalentim79@gmail.com>
2025-06-26 20:30:35 -0300

    Projeto Finalizado

--- src/main/java/org/example/ContaTerminal.java
+++ src/main/java/org/example/ContaTerminal.java
@@ -1,10 +1,36 @@
 package org.example;
 
 
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+     /*
+     * Atributos:
+     * Numero da Conta - Integer
+     * Agencia - String
+     * NomeCliente - String
+     * Saldo - Double
+     * */
 
+        String texto1 =", obrigado por criar uma conta em nosso banco, sua agência é ";
+
+        var scanner = new Scanner(System.in);
+
+        System.out.print("Por Favor, digite o nome: ");
+        var nome = scanner.nextLine();
+
+        System.out.print("Por Favor, digite o numero da Agencia: ");
+        var agencia = scanner.nextLine();
+
+        System.out.print("Por Favor, digite o numero da Conta: ");
+        var conta = scanner.nextInt();
+
+        System.out.print("Por Favor, digite o saldo da Conta: ");
+        var saldo = scanner.nextFloat();
+
+
+        System.out.println("Olá " +nome.concat(texto1) +agencia+ " conta " +conta+ " e seu saldo é " +saldo+ " já está disponível para saque");
 
     }
 }
